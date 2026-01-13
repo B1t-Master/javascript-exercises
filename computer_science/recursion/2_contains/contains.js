e02ed81cb03099008e10302886dd216665ac4917
B1t-Master <isaac.njuguna@strathmore.edu>
2026-01-13 21:15:07 +0300

    Finish qn2

--- computer_science/recursion/2_contains/contains.js
+++ computer_science/recursion/2_contains/contains.js
@@ -1,6 +1,13 @@
-const contains = function() {
-  
+const contains = function (object, value) {
+  if (object === null) return false;
+
+  let items = Object.values(object);
+  if (items.includes(value)) return true;
+
+  return items
+    .filter((key) => typeof key === "object" && key !== null)
+    .some((e) => contains(e, value));
 };
-  
+
 // Do not edit below this line
 module.exports = contains;
