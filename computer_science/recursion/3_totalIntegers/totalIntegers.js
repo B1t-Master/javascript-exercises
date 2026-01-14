cf6c73f5afffe3a9029ef29345602821dcf49370
B1t-Master <isaac.njuguna@strathmore.edu>
2026-01-14 11:25:47 +0300

    Finish qn3

--- computer_science/recursion/3_totalIntegers/totalIntegers.js
+++ computer_science/recursion/3_totalIntegers/totalIntegers.js
@@ -1,6 +1,19 @@
-const totalIntegers = function() {
-  
+const totalIntegers = function (dataStructure) {
+  let total = 0;
+  if (!(typeof dataStructure === "object" && dataStructure !== null))
+    return undefined;
+  array = Object.values(dataStructure);
+  array
+    .filter(
+      (item) =>
+        (typeof item === "object" && item !== null) || Number.isInteger(item)
+    )
+    .map((item) => {
+      if (Number.isInteger(item)) return total++;
+      else return (total += totalIntegers(item));
+    }, total);
+  return total;
 };
-  
+
 // Do not edit below this line
 module.exports = totalIntegers;
