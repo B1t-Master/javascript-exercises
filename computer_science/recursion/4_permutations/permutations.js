b361f095355e78ced5bab9158a227d797da1017c
B1t-Master <isaac.njuguna@strathmore.edu>
2026-01-15 10:29:39 +0300

    Attempt qn4

--- computer_science/recursion/4_permutations/permutations.js
+++ computer_science/recursion/4_permutations/permutations.js
@@ -1,6 +1,19 @@
-const permutations = function() {
-  
+//heavily used assistance needs to be looked into at a later time
+
+const permutations = function (array, tempArray = [], resultArray = []) {
+  // resultArray.push(array);
+  if (!array.length) resultArray.push([...tempArray]);
+  // if (array.length === 1) return [array];
+
+  for (let i = 0; i < array.length; i++) {
+    const newArray = array.filter((n, index) => index !== i);
+    tempArray.push(array[i]);
+    permutations(newArray, tempArray, resultArray);
+    tempArray.pop();
+  }
+
+  return resultArray;
 };
-  
+
 // Do not edit below this line
 module.exports = permutations;
