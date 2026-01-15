9afaaf92d877f355c5aa6d80a55efbe1816bbcb2
B1t-Master <isaac.njuguna@strathmore.edu>
2026-01-15 11:50:54 +0300

    Finish qn5

--- computer_science/recursion/5_pascal/pascal.js
+++ computer_science/recursion/5_pascal/pascal.js
@@ -1,6 +1,19 @@
-const pascal = function() {
-  
+const pascal = function (n, array = []) {
+  if (n === 1) {
+    array.push(1);
+    return array;
+  }
+  pascal(n - 1, array);
+
+  array.push(0);
+  array.unshift(0);
+  array.map((element, index, array) => {
+    if (index === array.length - 1) return;
+    array[index] = array[index] + array[index + 1];
+  });
+
+  return array.filter((index) => index != 0);
 };
-  
+
 // Do not edit below this line
 module.exports = pascal;
